ebcc9ffadc92064c7d35e526a11bbc353ca4320c
qayyamj <qayyamj@gmail.com>
2021-01-26 09:41:56 -0500

    created splitFullName function in contact class

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,27 +1,52 @@
-class Contact {
+class Contact 
+{
     // Constructor
-    constructor(fullName, contactNumber, emailAddress) {
+    constructor(fullName, contactNumber, emailAddress) 
+    {
         this.m_fullName = fullName;
         this.m_contactNumber = contactNumber;
         this.m_emailAddress = emailAddress;
+
+        this.splitFullName();
     }
     // Getters and Setters
-    get fullName() {
+    get fullName() 
+    {
         return this.m_fullName;
     }
-    get contactNumber() {
+
+    get contactNumber() 
+    {
         return this.contactNumber;
     }
-    get emailAddress() {
+
+    get emailAddress() 
+    {
         return this.emailAddress;
     }
-    set fullName(full_name) {
+
+    set fullName(full_name) 
+    {
         this.m_fullName = full_name;
     }
-    set contactNumber(contact_number) {
+
+    set contactNumber(contact_number) 
+    {
         this.m_contactNumber = contact_number;
     }
-    set emailAddress(email_address) {
+
+    set emailAddress(email_address)
+    {
         this.m_emailAddress = email_address;
     }
+
+    // functions
+    splitFullName()
+    {
+        
+        let names = this.m_fullName.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
+
 }
